079351b179df66de008c216ce3356bf86697e8e0
Aiden Vaines <aiden.vaines1@nhs.net>
2019-07-03 11:22:01 +0100

    fixing

--- terraform/modules/bastion_host/ec2.tf
+++ terraform/modules/bastion_host/ec2.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "bastion-host" {
   ami = "${data.aws_ami.amazon-linux-2.id}"
   instance_type = "${var.size}"
   key_name = "${var.key_pair}"
-  vpc_security_group_ids = ["${aws_security_group.bastion_host_sg.id}"]
+  vpc_security_group_ids = ["${aws_security_group.host_access.id}"]
 
   tags = "${merge(var.tag_map, map(
     "Name", "${var.hostname}"
