21ee3724cca77e10e3b09aed329d21b091199f46
Aiden Vaines <aiden.vaines1@nhs.net>
2019-07-03 13:01:37 +0100

    fixing bastion outputs

--- terraform/modules/bastion_host/outputs.tf
+++ terraform/modules/bastion_host/outputs.tf
@@ -1,7 +1,15 @@
-output "name" {
-  value = "${aws_instance.bastion-host.name}"
+output "id" {
+  value = "${aws_instance.bastion-host.id}"
+}
+
+output "arn" {
+  value = "${aws_instance.bastion-host.arn}"
 }
 
 output "private_ip" {
   value = "${aws_instance.bastion-host.private_ip}"
+}
+
+output "public_ip" {
+  value = "${aws_instance.bastion-host.public_ip}"
 }
